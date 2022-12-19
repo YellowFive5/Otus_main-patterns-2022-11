05788b5a39e44dc067d4117499e2301a02ea8226
YellowFive <yellowfive555@yahoo.com>
2022-12-19 17:02:11 +0300

    Cant get object position test fails

--- Move.Tests/WhenMovingObjects.cs
+++ Move.Tests/WhenMovingObjects.cs
@@ -1,5 +1,6 @@
 #region Usings
 
+using System;
 using System.Numerics;
 using FluentAssertions;
 using NUnit.Framework;
@@ -11,7 +12,7 @@ namespace Move.Tests
     public class WhenMovingObjects : TestBase
     {
         [TestCase(12, 5, -7, 3, 5, 8)]
-        public void ObjectMoves(int posX, int posY, int velX, int velY, int expectedPosX, int expectedPosY)
+        public void ObjectMoves(float posX, float posY, float velX, float velY, float expectedPosX, float expectedPosY)
         {
             var objectToMove = new SpaceShip
                                {
@@ -23,6 +24,37 @@ namespace Move.Tests
             mover.Execute();
 
             objectToMove.Position.Should().Be(new Vector2(expectedPosX, expectedPosY));
+        }
+
+        [TestCase(float.NaN, 0)]
+        [TestCase(float.NegativeInfinity, 0)]
+        [TestCase(float.PositiveInfinity, 0)]
+        [TestCase(0, float.NaN)]
+        [TestCase(0, float.NegativeInfinity)]
+        [TestCase(0, float.PositiveInfinity)]
+        public void ExceptionThrowsWhenCantGetPosition(float posX, float posY)
+        {
+            var objectToMove = new SpaceShip { Position = new Vector2(posX, posY) };
+            var mover = new Move(objectToMove);
+
+            Action act = () => mover.Execute();
+
+            act.Should()
+               .Throw<Exception>()
+               .WithMessage("Can't get object position");
+        }
+
+        [Test]
+        public void ExceptionThrowsWhenMovableObjectIsNull()
+        {
+            IMovable objectToMove = null;
+            var mover = new Move(objectToMove);
+
+            Action act = () => mover.Execute();
+
+            act.Should()
+               .Throw<Exception>()
+               .WithMessage("Can't get object position");
         }
     }
 }
