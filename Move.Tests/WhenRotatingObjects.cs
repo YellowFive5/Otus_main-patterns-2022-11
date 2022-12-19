324a640d3e791f5c6fd746211910b1b120bbc478
YellowFive <yellowfive555@yahoo.com>
2022-12-19 18:36:05 +0300

    Can't rotate object test fails

--- Move.Tests/WhenRotatingObjects.cs
+++ Move.Tests/WhenRotatingObjects.cs
@@ -1,6 +1,26 @@
-﻿namespace Move.Tests
+﻿#region Usings
+
+using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+#endregion
+
+namespace Move.Tests
 {
     public class WhenRotatingObjects : TestBase
     {
+        [Test]
+        public void ExceptionThrowsWhenRotatableObjectIsNull()
+        {
+            IRotatable objectToRotate = null;
+            var mover = new Rotate(objectToRotate);
+
+            Action act = () => mover.Execute();
+
+            act.Should()
+               .Throw<Exception>()
+               .WithMessage("Can't rotate object");
+        }
     }
 }
