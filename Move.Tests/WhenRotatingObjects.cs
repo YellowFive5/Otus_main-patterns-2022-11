1c2564ed9071a9555f6a6ef8524a97fc36ce6a23
YellowFive <yellowfive555@yahoo.com>
2022-12-19 19:06:03 +0300

    ObjectRotates tests fails

--- Move.Tests/WhenRotatingObjects.cs
+++ Move.Tests/WhenRotatingObjects.cs
@@ -2,6 +2,7 @@
 
 using System;
 using FluentAssertions;
+using Moq;
 using NUnit.Framework;
 
 #endregion
@@ -21,6 +22,20 @@ namespace Move.Tests
             act.Should()
                .Throw<Exception>()
                .WithMessage("Can't rotate object");
+        }
+
+        [TestCase(0, 16, 8, 8)]
+        public void ObjectRotates(int startDir, int angVel, int dir, int expectedDir)
+        {
+            var objectToRotate = new Mock<IRotatable>();
+            objectToRotate.SetupGet(o => o.Direction).Returns(startDir);
+            objectToRotate.SetupGet(o => o.AngularVelocity).Returns(angVel);
+            objectToRotate.SetupGet(o => o.Direction).Returns(dir);
+            var rotator = new Rotate(objectToRotate.Object);
+
+            rotator.Execute();
+
+            objectToRotate.VerifySet(o => o.Direction = expectedDir);
         }
     }
 }
