e698f6d8a73b076e3b0fb96fa94ac2ab786661f1
YellowFive <yellowfive555@yahoo.com>
2022-12-19 17:02:24 +0300

    Cant get object position realization

--- Move/Move.cs
+++ Move/Move.cs
@@ -1,4 +1,10 @@
-﻿namespace Move
+﻿#region Usings
+
+using System;
+
+#endregion
+
+namespace Move
 {
     public class Move
     {
@@ -11,6 +17,15 @@
 
         public void Execute()
         {
+            if (toMove == null ||
+                float.IsInfinity(toMove.Position.X) ||
+                float.IsInfinity(toMove.Position.Y) ||
+                float.IsNaN(toMove.Position.X) ||
+                float.IsNaN(toMove.Position.Y))
+            {
+                throw new Exception("Can't get object position");
+            }
+
             toMove.Position += toMove.Velocity;
         }
     }
