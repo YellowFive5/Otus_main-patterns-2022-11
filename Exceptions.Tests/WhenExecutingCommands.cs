9edaa2250b8a1234e678b436ee088e92e1af0fb8
YellowFive <yellowfive555@yahoo.com>
2022-12-28 16:13:51 +0300

    LogCommandExecutes tests fails

--- Exceptions.Tests/WhenExecutingCommands.cs
+++ Exceptions.Tests/WhenExecutingCommands.cs
@@ -1,10 +1,24 @@
 #region Usings
 
+using FluentAssertions;
+using NUnit.Framework;
+
 #endregion
 
 namespace Exceptions.Tests
 {
     public class WhenExecutingCommands : TestBase
     {
+        [Test]
+        public void LogCommandExecutes()
+        {
+            var logCommand = new LogCommand();
+
+            logCommand.LogMessage.Should().Be("No message");
+
+            logCommand.Execute();
+
+            logCommand.LogMessage.Should().Be("New logged message");
+        }
     }
 }
---
+++ Exceptions/LogCommand.cs
@@ -0,0 +1,15 @@
+﻿#region Usings
+
+#endregion
+
+namespace Exceptions
+{
+    public class LogCommand : ICommand
+    {
+        public string LogMessage { get; private set; } = "No message";
+
+        public void Execute()
+        {
+        }
+    }
+}
