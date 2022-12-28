92154d65cc373ed2de879e4b692d9d08a603ac6f
YellowFive <yellowfive555@yahoo.com>
2022-12-28 16:48:39 +0300

    RetryCommandExecutes realization

---
+++ Exceptions/RetryCommand.cs
@@ -0,0 +1,21 @@
+﻿#region Usings
+
+#endregion
+
+namespace Exceptions
+{
+    public class RetryCommand : ICommand
+    {
+        private readonly ICommand commandToRetry;
+
+        public RetryCommand(ICommand commandToRetry)
+        {
+            this.commandToRetry = commandToRetry;
+        }
+
+        public void Execute()
+        {
+            commandToRetry.Execute();
+        }
+    }
+}
