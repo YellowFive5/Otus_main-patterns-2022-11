3161c8e7fda7ba590f756ff39c065d13bc260cb6
YellowFive <yellowfive555@yahoo.com>
2022-12-30 16:53:43 +0300

    ExecutingStopsOnFailedCommand test fails

--- Command.Tests/WhenExecutingMacroCommand.cs
+++ Command.Tests/WhenExecutingMacroCommand.cs
@@ -1,6 +1,8 @@
 ﻿#region Usings
 
+using System;
 using Exceptions.Commands;
+using FluentAssertions;
 using Moq;
 using NUnit.Framework;
 
@@ -21,6 +23,23 @@ namespace Command.Tests
 
             command1.Verify(fc => fc.Execute(), Times.Once);
             command2.Verify(fc => fc.Execute(), Times.Once);
+        }
+
+        [Test]
+        public void ExecutingStopsOnFailedCommand()
+        {
+            var command1 = new Mock<ICommand>();
+            var failedCommand = new Mock<ICommand>();
+            failedCommand.Setup(c => c.Execute()).Throws<Exception>();
+            var command3 = new Mock<ICommand>();
+            var macroCommand = new MacroCommand(command1.Object, failedCommand.Object, command3.Object);
+
+            Action act = () => macroCommand.Execute();
+
+            act.Should().Throw<CommandException>();
+            command1.Verify(fc => fc.Execute(), Times.Once);
+            failedCommand.Verify(fc => fc.Execute(), Times.Once);
+            command3.Verify(fc => fc.Execute(), Times.Never);
         }
     }
 }
