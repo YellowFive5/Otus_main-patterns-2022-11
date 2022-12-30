4efa756dab1225000fd1c87f2045d5d3f50f0d3c
YellowFive <yellowfive555@yahoo.com>
2022-12-30 16:46:04 +0300

    WhenExecutingMacroCommand tests fails

---
+++ Command.Tests/WhenExecutingMacroCommand.cs
@@ -0,0 +1,26 @@
+﻿#region Usings
+
+using Exceptions.Commands;
+using Moq;
+using NUnit.Framework;
+
+#endregion
+
+namespace Command.Tests
+{
+    public class WhenExecutingMacroCommand : TestBase
+    {
+        [Test]
+        public void AllCommandsExecutes()
+        {
+            var command1 = new Mock<ICommand>();
+            var command2 = new Mock<ICommand>();
+            var macroCommand = new MacroCommand(command1.Object, command2.Object);
+
+            macroCommand.Execute();
+
+            command1.Verify(fc => fc.Execute(), Times.Once);
+            command2.Verify(fc => fc.Execute(), Times.Once);
+        }
+    }
+}
---
+++ Command/MacroCommand.cs
@@ -0,0 +1,24 @@
+﻿#region Usings
+
+using System;
+using Exceptions.Commands;
+
+#endregion
+
+namespace Command
+{
+    public class MacroCommand : ICommand
+    {
+        private readonly ICommand[] commands;
+
+        public MacroCommand(params ICommand[] commands)
+        {
+            this.commands = commands;
+        }
+
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
