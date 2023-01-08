dcaedf8847f616b3f8c6ce246864a18a7b5a41b9
YellowFive <yellowfive555@yahoo.com>
2023-01-08 14:09:19 +0300

    RepeatableCommandRepeatExecuting test fails

---
+++ Command.Tests/WhenExecutingRepeatableCommand.cs
@@ -0,0 +1,24 @@
+﻿#region Usings
+
+using Exceptions.Commands;
+using Moq;
+using NUnit.Framework;
+
+#endregion
+
+namespace Command.Tests
+{
+    public class WhenExecutingRepeatableCommand : TestBase
+    {
+        [Test]
+        public void RepeatableCommandRepeatExecuting()
+        {
+            var commandToRepeat = new Mock<ICommand>();
+            var repeatableCommand = new RepeatableCommand(commandToRepeat.Object);
+
+            repeatableCommand.Execute();
+
+            commandToRepeat.Verify(fc => fc.Execute(), Times.AtLeast(2));
+        }
+    }
+}
---
+++ Command/RepeatableCommand.cs
@@ -0,0 +1,24 @@
+﻿#region Usings
+
+using System;
+using Exceptions.Commands;
+
+#endregion
+
+namespace Command
+{
+    public class RepeatableCommand : ICommand
+    {
+        private readonly ICommand commandToRepeat;
+
+        public RepeatableCommand(ICommand commandToRepeat)
+        {
+            this.commandToRepeat = commandToRepeat;
+        }
+
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
