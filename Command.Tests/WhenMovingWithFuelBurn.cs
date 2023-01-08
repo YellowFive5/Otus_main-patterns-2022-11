17b00be27d068ac305a0f18627b9cb4829678f4a
YellowFive <yellowfive555@yahoo.com>
2023-01-08 15:31:33 +0300

    ObjectMovesOnRepeatTillEnoughFuel test add

--- Command.Tests/WhenMovingWithFuelBurn.cs
+++ Command.Tests/WhenMovingWithFuelBurn.cs
@@ -1,7 +1,10 @@
 ﻿#region Usings
 
 using System;
+using System.Collections.Generic;
 using System.Numerics;
+using Exceptions;
+using Exceptions.Commands;
 using FluentAssertions;
 using Moq;
 using Move;
@@ -56,6 +59,34 @@ namespace Command.Tests
             objectToMoveAndBurn.VerifyGet(o => o.FuelConsumption);
             objectToMove.VerifySet(o => o.Position = It.IsAny<Vector2>(), Times.Never);
             objectToMoveAndBurn.VerifySet(o => o.FuelLevel = It.IsAny<int>(), Times.Never);
+        }
+
+        [Test]
+        public void ObjectMovesOnRepeatTillEnoughFuel()
+        {
+            var objectToMove = new Mock<IMovable>();
+            var objectToMoveAndBurn = objectToMove.As<IFuelBurnable>();
+            objectToMove.SetupProperty(o => o.Position);
+            objectToMove.Object.Position = new Vector2(12, 5);
+            objectToMove.Setup(o => o.Velocity).Returns(new Vector2(-7, 3));
+            objectToMoveAndBurn.SetupProperty(o => o.FuelLevel);
+            objectToMoveAndBurn.Object.FuelLevel = 100;
+            objectToMoveAndBurn.Setup(o => o.FuelConsumption).Returns(5);
+            var checkFuelCommand = new CheckFuelCommand(objectToMoveAndBurn.Object);
+            var moveCommand = new MoveCommand(objectToMove.Object);
+            var burnFuelCommand = new BurnFuelCommand(objectToMoveAndBurn.Object);
+            var moveAndBurnMacroCommand = new MacroCommand(checkFuelCommand, moveCommand, burnFuelCommand);
+            var commands = new Queue<ICommand>();
+            var repeatableMoveAndBurnMacroCommand = new RepeatableCommand(moveAndBurnMacroCommand, commands);
+            commands.Enqueue(repeatableMoveAndBurnMacroCommand);
+            var server = new Server(commands, new Mock<ILogger>().Object);
+
+            server.RunCommandsTillFirstException();
+
+            objectToMoveAndBurn.VerifySet(o => o.FuelLevel = It.IsAny<int>(), Times.Exactly(21));
+            objectToMoveAndBurn.VerifySet(o => o.FuelLevel = 0, Times.Once);
+            objectToMove.VerifySet(o => o.Position = It.IsAny<Vector2>(), Times.Exactly(21));
+            objectToMove.VerifySet(o => o.Position = new Vector2(-128, 65), Times.Once);
         }
     }
 }
