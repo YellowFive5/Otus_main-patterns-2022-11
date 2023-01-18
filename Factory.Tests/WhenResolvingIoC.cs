469ab3b14f7a4aa4a6b3277229f5be6a7e753662
YellowFive <yellowfive555@yahoo.com>
2023-01-18 15:23:27 +0300

    ErrorThrowsWhenOperationKeyNotFound test fails

---
+++ Factory.Tests/WhenResolvingIoC.cs
@@ -0,0 +1,26 @@
+﻿#region Usings
+
+using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+#endregion
+
+namespace Factory.Tests
+{
+    public class WhenResolvingIoC : TestBase
+    {
+        [TestCase(null)]
+        [TestCase("NoOperationWithThisKey")]
+        public void ErrorThrowsWhenOperationKeyNotFound(string operationName)
+        {
+            var ioc = new IoC();
+
+            Action act = () => ioc.Resolve<object>(operationName);
+
+            act.Should()
+               .Throw<Exception>()
+               .WithMessage($"No operation with key {operationName}");
+        }
+    }
+}
