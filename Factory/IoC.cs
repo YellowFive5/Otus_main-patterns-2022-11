7f4bb3044f4c4de7a05e895d3701f645ff02ba1e
YellowFive <yellowfive555@yahoo.com>
2023-01-22 14:01:14 +0300

    VelocityGetsWithMovableAdapter realization

--- Factory/IoC.cs
+++ Factory/IoC.cs
@@ -17,6 +17,13 @@ namespace Factory
 
         public T Resolve<T>(string key, params object[] args)
         {
+            if (key == "Object.GetVelocity")
+            {
+                return ((IMovable)args[0]).Velocity is T velocity
+                           ? velocity
+                           : default;
+            }
+
             if (key == "Object.GetPosition")
             {
                 return ((IMovable)args[0]).Position is T position
