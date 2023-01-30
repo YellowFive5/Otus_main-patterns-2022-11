21542acd844726fb7274f30be27ab8c12c3468e3
YellowFive <yellowfive555@yahoo.com>
2023-01-30 14:16:36 +0300

    MultithreadExecutionRunning test fails

---
+++ Multithreading.Tests/WhenRunMultithreadedQueue.cs
@@ -0,0 +1,33 @@
+﻿#region Usings
+
+using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using Exceptions;
+using Exceptions.Commands;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+
+#endregion
+
+namespace Multithreading.Tests
+{
+    public class WhenRunMultithreadedQueue : TestBase
+    {
+        [Test]
+        public void MultithreadExecutionRunning()
+        {
+            var testCommand = new Mock<ICommand>();
+            var mre = new ManualResetEvent(false);
+            testCommand.Setup(c => c.Execute()).Callback(() => mre.Set());
+            var queue = new ConcurrentQueue<ICommand>();
+            queue.Enqueue(testCommand.Object);
+            var server = new Server(queue, Logger.Object);
+            
+            server.RunMultithreadCommands();
+
+            mre.WaitOne(TimeSpan.FromSeconds(1)).Should().BeTrue();
+        }
+    }
+}
