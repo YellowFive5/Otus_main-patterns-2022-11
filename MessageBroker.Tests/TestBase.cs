fe98e39183df9d726da4d6d22c2a54e95c5eab96
YellowFive <yellowfive555@yahoo.com>
2023-02-05 11:43:39 +0300

    ObjectMovesAfterServerReceivingMessage test fails

---
+++ MessageBroker.Tests/TestBase.cs
@@ -0,0 +1,21 @@
+﻿#region Usings
+
+using Moq;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+#endregion
+
+namespace MessageBroker.Tests
+{
+    public class TestBase
+    {
+        protected Mock<ILogger> Logger { get; set; }
+
+        [SetUp]
+        public virtual void Setup()
+        {
+            Logger = new Mock<ILogger>();
+        }
+    }
+}
---
+++ MessageBroker.Tests/WhenServerReceivesMessage.cs
@@ -0,0 +1,25 @@
+﻿#region Usings
+
+using System.Numerics;
+using Moq;
+using Move;
+using NUnit.Framework;
+
+#endregion
+
+namespace MessageBroker.Tests
+{
+    public class WhenServerReceivesMessage : TestBase
+    {
+        [Test]
+        public void ObjectMovesAfterServerReceivingMessage()
+        {
+            var objectToMove = new Mock<IMovable>();
+            objectToMove.SetupGet(o => o.Position).Returns(new Vector2(12, 5));
+            objectToMove.SetupGet(o => o.Velocity).Returns(new Vector2(-7, 3));
+            
+            
+            objectToMove.VerifySet(o => o.Position = new Vector2(5, 8));
+        }
+    }
+}
