2ad203679dd70d375c8e14108b2a30d89ad62339
YellowFive <yellowfive555@yahoo.com>
2023-02-22 15:33:42 +0300

    BattleAuthorizationTokenCorrectOnlyForAuthorizedInBattleUsers test fails

--- Authorization.Tests/WhenAuthorizing.cs
+++ Authorization.Tests/WhenAuthorizing.cs
@@ -15,9 +15,9 @@ namespace Authorization.Tests
         [TestCase(99, "26asd*!", false)]
         public void AuthorizationTokenCorrectWhenPassCorrect(int userId, string userPass, bool expected)
         {
-            var user = new User { Id = userId };
             var authorizationService = new AuthorizationService();
-            
+            var user = new User { Id = userId };
+
             var authorizationToken = authorizationService.GetAuthorizeToken(user.Id, userPass);
 
             authorizationService.CheckAuthorizationTokenCorrect(authorizationToken).Should().Be(expected);
@@ -28,15 +28,35 @@ namespace Authorization.Tests
         [TestCase(99, "26asd*!", false)]
         public void AuthorizedUserCanStartBattle(int userId, string userPass, bool expected)
         {
+            var authorizationService = new AuthorizationService();
             var user = new User { Id = userId };
             var battleUserIds = new[] { 1, 2, 3 };
-            var authorizationService = new AuthorizationService();
             var authorizationToken = authorizationService.GetAuthorizeToken(user.Id, userPass);
 
             var battleId = authorizationService.RegisterBattle(authorizationToken, battleUserIds);
 
             (battleId != null).Should().Be(expected);
             (authorizationService.Battles.Count() != 0).Should().Be(expected);
+        }
+
+        [TestCase(1, "User_1_Pass_!", true)]
+        [TestCase(2, "User_2_Pass_!", true)]
+        [TestCase(3, "User_3_Pass_!", true)]
+        [TestCase(4, "User_4_Pass_!", false)]
+        [TestCase(99, "26asd*!", false)]
+        public void BattleAuthorizationTokenCorrectOnlyForAuthorizedInBattleUsers(int userId, string userPass, bool expected)
+        {
+            var authorizationService = new AuthorizationService();
+            var serverUser = new User { Id = 1 };
+            var battleUserIds = new[] { 1, 2, 3 };
+            var authorizationToken = authorizationService.GetAuthorizeToken(serverUser.Id, "User_1_Pass_!");
+            var battleId = authorizationService.RegisterBattle(authorizationToken, battleUserIds);
+            var testUser = new User { Id = userId };
+            authorizationToken = authorizationService.GetAuthorizeToken(testUser.Id, userPass);
+
+            var battleAuthorizationToken = authorizationService.GetBattleAuthorizeToken(authorizationToken, battleId);
+
+            authorizationService.CheckBattleAuthorizationTokenCorrect(battleAuthorizationToken).Should().Be(expected);
         }
     }
 }
