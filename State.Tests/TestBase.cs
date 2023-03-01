fe9483fea067fee1f7f9aa05cbae0be0d5ce23f6
YellowFive <yellowfive555@yahoo.com>
2023-03-01 15:16:28 +0300

    init

---
+++ State.Tests/TestBase.cs
@@ -0,0 +1,21 @@
+﻿#region Usings
+
+using Exceptions;
+using Moq;
+using NUnit.Framework;
+
+#endregion
+
+namespace State.Tests
+{
+    public class TestBase
+    {
+        protected Mock<ILogger> Logger { get; set; }
+
+        [SetUp]
+        public virtual void Setup()
+        {
+            Logger = new Mock<ILogger>();
+        }
+    }
+}
