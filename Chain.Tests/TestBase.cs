d9aaa7a3174aab093c43df55ef9f573e548d1cf7
YellowFive <yellowfive555@yahoo.com>
2023-03-08 14:16:43 +0300

    projs init

---
+++ Chain.Tests/TestBase.cs
@@ -0,0 +1,25 @@
+﻿#region Usings
+
+using Exceptions;
+using Factory;
+using Moq;
+using NUnit.Framework;
+
+#endregion
+
+namespace Chain.Tests
+{
+    public class TestBase
+    {
+        protected Mock<ILogger> Logger { get; set; }
+        protected Mock<IResolvable> Ioc { get; set; }
+
+
+        [SetUp]
+        public virtual void Setup()
+        {
+            Logger = new Mock<ILogger>();
+            Ioc = new Mock<IResolvable>();
+        }
+    }
+}
